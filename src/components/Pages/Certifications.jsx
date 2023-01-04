7130d925032b736809e8e93790f02764ee036215
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-01-04 23:38:18 +0000

    adds certificates, the Certificates Component needs an upgrade

--- src/components/Pages/Certifications.jsx
+++ src/components/Pages/Certifications.jsx
@@ -29,23 +29,41 @@ function Certifications() {
         <li>
           <a href="https://www.udemy.com/certificate/UC-TM2EH9W3/" target="_blank">
             <span className="fa-li"><i className="fas fa-award text-warning"></i></span>
-            Complete Python Bootcamp From Zero to Hero in Python
+            Complete Python Bootcamp: From Zero to Hero in Python
+          </a>
+        </li>
+        <li>
+          <a href="https://www.udemy.com/certificate/UC-353e0a8a-3f2d-4b75-8443-307e4a60f9f6/" target="_blank">
+            <span className="fa-li"><i className="fas fa-award text-warning"></i></span>
+            Python for OOP: The A-to-Z OOP Python Programming Course
           </a>
         </li>
 
-
         <li>
-          <a href="https://www.udemy.com/certificate/UC-31ffd260-d337-40d9-b58e-a82ff6dada95/?utm_source=sendgrid.com&utm_medium=email&utm_campaign=email" target="_blank">
+          <a href="https://www.udemy.com/certificate/UC-ed7b00cf-1314-4fda-b867-052406e90486/" target="_blank">
+            <span className="fa-li"><i className="fas fa-award text-warning"></i></span>
+            React.JS: The Complete Course for Beginners 
+          </a>
+        </li>
+        <li>
+          <a href="https://www.udemy.com/certificate/UC-353e0a8a-3f2d-4b75-8443-307e4a60f9f6/" target="_blank">
+            <span className="fa-li"><i className="fas fa-award text-warning"></i></span>
+            JavaScript: The Complete Course for Beginners
+          </a>
+        </li>
+        <li>
+          <a href="https://www.udemy.com/certificate/UC-31ffd260-d337-40d9-b58e-a82ff6dada95/" target="_blank">
             <span className="fa-li"><i className="fas fa-award text-warning"></i></span>
             Bootstrap 5 Course: Build Responsive Websites 
           </a>
         </li>
         <li>
-          <a href="https://www.udemy.com/certificate/UC-f8cb5014-1f28-4f8a-a942-479cda877c4f/?utm_source=sendgrid.com&utm_medium=email&utm_campaign=email" target="_blank">
+          <a href="https://www.udemy.com/certificate/UC-f8cb5014-1f28-4f8a-a942-479cda877c4f/" target="_blank">
             <span className="fa-li"><i className="fas fa-award text-warning"></i></span>
             HTML5 & CSS3 Complete Course: Build Websites 
           </a>
         </li>
+
         <li>
           <a href="https://www.udemy.com/certificate/UC-7248e9db-045b-42d2-b3c6-d08e79df61bf/?utm_source=sendgrid.com&utm_medium=email&utm_campaign=email" target="_blank">
             <span className="fa-li"><i className="fas fa-award text-warning"></i></span>
