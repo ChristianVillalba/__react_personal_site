b8176deb1d84db99fc6bf5dda7f20603a7258155
Christian Villalba UK <christianvillalba.uk@gmail.com>
2024-04-19 21:07:52 +0100

    adds router and adds routes to apps.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,17 +1,30 @@
-import './App.css';
-import AboutSection from './components/AboutSection';
-import CertificateSection from './components/CertificateSection';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import SkillsSection from './components/SkillsSection';
+import Navbar from "./components/Navbar";
+// Routes
+import AboutSection from "./components/Pages/AboutSection";
+import SkillsSection from "./components/Pages/SkillsSection";
+import PortfolioSection from "./components/Pages/PortfolioSection";
+import CertificateSection from "./components/Pages/CertificateSection";
+import InterestsSection from "./components/Pages/InterestsSection";
+
+import AboutSection from "./components/Pages/AboutSection";
 
 function App() {
   return (
     <div>
-      <Navbar ></Navbar>
-      <AboutSection></AboutSection>
-      <SkillsSection></SkillsSection>
-      <CertificateSection></CertificateSection>
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<AboutSection />} />
+          <Route path="skills" element={<SkillsSection />} />
+          <Route path="portfolio" element={<PortfolioSection />} />
+          <Route path="certificates" element={<CertificateSection />} />
+          <Route path="interests" element={<InterestsSection />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
